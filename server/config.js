48ebbee22af6342987826d2ed8b72143fa8bc101
刘宇飞 <liuyufei@tongtongmall.com>
2018-11-20 17:24:47 +0800

    todo

---
+++ server/config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by fly on 2017/4/13.
+ * 常用的一些字段
+ */
+ exports.option = {
+    host: '101.200.33.212',
+    user: 'root',
+    password: '123456',
+    database: 'test'
+}
+
--- server/routers.js
+++ server/routers.js
@@ -3,10 +3,10 @@ var router = express.Router();
 var db = require('./db')
 
 router.get('/', (req, res) => {
-    // console.log(req.query)
-    // db.findOne('account', {}, function (err, res) {
-    //     console.log(res);
-    // });
+    db.query('select * from account')
+        .then(d => {
+            console.log(d)
+        })
     res.send({
         code: 1100,
         msg: '',
@@ -26,13 +26,13 @@ router.post('/post', (req, res) => {
 * @description 登陆
 **/
 router.post('/user/login', (req, res) => {
-    db.findOne('account', {name: req.body.account}, function (err, data) {
-        res.send({
-            code: data && data.password === req.body.password ? 1100 : 1101,
-            msg: data && data.password === req.body.password ? '' : '用户名或密码错误',
-            data:''
+    db.query(`select * from account where name = "${req.body.account}"`)
+        .then(data => {
+            res.send({
+                code: data && data[0].password == req.body.password ? 1100 : 1101,
+                msg: data && data[0].password == req.body.password ? '' : '用户名或密码错误',
+                data:''
+            })
         })
-
-    });
 })
 module.exports = router;
