eecc05c373037d812376ab78a57125b66da72bb8
fly <1397091912@qq.com>
2018-11-21 22:30:41 +0800

    todo

--- server/routers.js
+++ server/routers.js
@@ -29,10 +29,37 @@ router.post('/user/login', (req, res) => {
     db.query(`select * from account where name = "${req.body.account}"`)
         .then(data => {
             res.send({
-                code: data && data[0].password == req.body.password ? 1100 : 1101,
-                msg: data && data[0].password == req.body.password ? '' : '用户名或密码错误',
-                data:''
+                code: data && data[0].password === req.body.password ? 1100 : 1101,
+                msg: data && data[0].password === req.body.password ? '' : '用户名或密码错误',
+                data: data && data[0].password === req.body.password ? '登陆成功' : '',
             })
         })
 })
+/**
+ * @description 注册
+ */
+router.post('/user/regist', (req, res) => {
+    db.query(`select * from account where name = "${req.body.account}"`)
+        .then(d => {
+            if(d.length > 0){
+                res.send({
+                    code: 1102,
+                    msg: '用户名已注册',
+                    data: ''
+                })
+            }else{
+                db.query(`insert into account (name, password) values("${req.body.account}", "${req.body.password}")`)
+                    .then(d => {
+                        if(d.affectedRows === 1) {
+                            res.send({
+                                code: 1100,
+                                msg: '',
+                                data: '注册成功'
+                            })
+                        }
+                    })
+            }
+        })
+})
+
 module.exports = router;
--- src/components/auth/regist.tsx
+++ src/components/auth/regist.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { NavLink  } from 'react-router-dom'
+import http from '../../lib/http'
 
 const Logins = styled.div`
     margin: 0.25rem 0.15rem;
@@ -23,6 +24,7 @@ const Logins = styled.div`
         color: #fff;
         font-size: 0.16rem;
         border-radius: 0.04rem;
+        cursor: pointer;
     }
     p{
         width: 100%;
@@ -30,25 +32,77 @@ const Logins = styled.div`
         span{
             font-size: 0.12rem;
             color: #999;
+            cursor: pointer;
         }
         .forget{
             float: right;
+            cursor: pointer;
+        }
+        .err-msg{
+            color: #d81e06;
         }
     }
 `
 
 export default class RegistTab extends React.Component<{}, {}> {
+    public state = {
+        name: '',
+        password: '',
+        repeatPass: '',
+        errMsg: ''
+    }
     public render() {
         return <Logins>
-            <input type="text" placeholder='输入您的用户名'/>
-            <input type="password" placeholder='输入您的密码'/>
-            <input type="password" placeholder='再次输入您的密码'/>
-            <button>注册</button>
+            <input type="text" placeholder='输入您的用户名'
+                value={this.state.name}
+                onChange={(e) => this.change(e, 'name')}    
+            />
+            <input type="password" placeholder='输入您的密码'
+                value={this.state.password}
+                onChange={(e) => this.change(e, 'password')}
+            />
+            <input type="password" placeholder='再次输入您的密码'
+                value={this.state.repeatPass}
+                onChange={(e) => this.change(e, 'repeatPass')}
+            />
+            <p>
+                <span className='err-msg'>{this.state.errMsg}</span>
+            </p>
+            <button onClick={this.regist}>注册</button>
             <p>
                 <span className='forget'>
                     <NavLink to="/login">去登陆</NavLink>
                 </span>
             </p>
         </Logins>
+    }
+    /**
+     * @description 输入框值改变
+     * @param e this，key：key
+     */
+    public change = (e: any, key: any) => {
+        const data: any = {}
+        data[key] = e.target.value
+        this.setState(data)
+    }
+    public regist = () => {
+        if(this.state.password !== this.state.repeatPass){
+            this.setState({
+                errMsg: '两次输入的密码不一致'
+            })
+            return
+        }
+        http.post('/user/regist', {
+            account: this.state.name,
+            password: this.state.password
+        }).then((d: any) => {
+            if(d.code === 1100){
+                window.location.href = '/login'
+            }else if(d.code === 1102) {
+                this.setState({
+                    errMsg: d.msg
+                })
+            }
+        })
     }
 }
